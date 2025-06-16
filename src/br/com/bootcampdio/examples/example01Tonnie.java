cc143e60f79758a88bf4c06a6a9fddc480a12c0e
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2025-06-16 19:39:40 -0300

    changes example01Tonnie

--- src/br/com/bootcampdio/examples/example01Tonnie.java
+++ src/br/com/bootcampdio/examples/example01Tonnie.java
@@ -16,14 +16,21 @@ public class example01Tonnie {
         System.out.println("Are you emancipated?: (y/n)");
         String isEmancipated = sc.nextLine().equalsIgnoreCase("y") ? "yes" : "no";
 
-        if (personAge >= 18) {
+        // short way
+        boolean canDrive = (personAge >= 18) || (personAge >= 16 && isEmancipated.equalsIgnoreCase("yes"));
+        String message = canDrive ?
+                personName + ", you can drive! \n":
+                personName + ", you can't drive! \n";
+        System.out.println(message);
+
+        /*if (personAge >= 18) {
             System.out.printf("%s you're %d, you can drive!", personName, personAge);
         } else if (personAge >= 16 && isEmancipated.equalsIgnoreCase("yes")) {
             System.out.printf("%s, although you are %d years old, you are emancipated and can drive!", personName, personAge);
         }
         else {
             System.out.printf("%s you're %d, you can't drive!", personName, personAge);
-        }
+        }*/
         sc.close();
     }
 }
