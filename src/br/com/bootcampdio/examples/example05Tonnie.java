e1f6e87d67d211338fa5edb62046da69b8539ccb
Gabriel Do Amaral Alves <skypse0136@gmail.com>
2025-06-23 19:29:13 -0300

    commit example05Tonnie.java and ex05Tonnie.java

---
+++ src/br/com/bootcampdio/examples/example05Tonnie.java
@@ -0,0 +1,7 @@
+package br.com.bootcampdio.examples;
+
+public class example05Tonnie {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/br/com/bootcampdio/exercisies/ex05Tonnie.java
@@ -0,0 +1,22 @@
+package br.com.bootcampdio.exercisies;
+
+import java.util.Scanner;
+
+public class ex05Tonnie {
+    public static void main(String[] args) {
+        // Write a code where the user enters a number and the table from 1 to 10 of that number is generated
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("----------_Multiplication_Table_----------");
+        // Enter a number
+        System.out.println("Please enter the number you want to make the multiplication table:");
+        int tabuadaNumber = sc.nextInt();
+        int contador = 1;
+
+        while (contador <= 10) {
+            System.out.printf("%d x %d = %d\n", tabuadaNumber, contador, tabuadaNumber * contador);
+            contador++;
+        }
+
+    }
+}
